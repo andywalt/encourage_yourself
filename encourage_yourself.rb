00c5485654b9a0bd29e3b7c895cc98ed366ff443
Andy Walters <andywalters42@gmail.com>
2018-03-09 16:47:40 -0800

    adjusted method for loop, class Person

--- encourage_yourself.rb
+++ encourage_yourself.rb
@@ -1,4 +1,8 @@
-feelings = ["anxiety", "depression", "discouragement", "unlovely"]
+
+@anxiety = ["Anxiety does not empty tomorrow of its sorrows, but only empties today of its strength. -Charles Spurgeon", "If you want to conquer the anxiety of life, live in the moment, live in the breath. - Amit Ray", "If you want to conquer the anxiety of life, live in the moment, live in the breath.", "Good humor is a tonic for mind and body. It is the best antidote for anxiety and depression. It is a business asset. It attracts and keeps friends. It lightens human burdens. It is the direct route to serenity and contentment. - Grenville Kleiser", "We all have anxiety about things. We all have little insecurities, but eventually you have to face your fears if you want to be successful, and everybody has some fear of failure. - Nick Saban"]
+@depression = ["Getting better from depression demands a lifelong commitment. I've made that commitment for my life's sake and for the sake of those who love me. — Susan Polis Schutz", "Once you choose hope, anything is possible. — Christopher Reeve", "A positive attitude gives you power over your circumstances instead of your circumstances having power over you. — Joyce Meyer", "Man is fond of counting his troubles, but he does not count his joys. If he counted them up as he ought to, he would see that every lot has enough happiness provided for it. — Fyodor Dostoevsky", "If you could only sense how important you are to the lives of those you meet; how important you can be to the people you may never even dream of. There is something of yourself that you leave at every meeting with another person. — Fred Rogers"]
+@discouragement = ["In spite of everything I shall rise again: I will take up my pencil, which I have forsaken in my great discouragement, and I will go on with my drawing. - Vincent Van Gogh", "We’re so busy watching out for what’s just ahead of us that we don’t take time to enjoy where we are. – Calvin & Hobbes", "Life is like riding a bicycle. To keep your balance, you must keep moving. – Albert Einstein", "The struggle you’re in today is developing the strength you need tomorrow. – Robert Tew"]
+@unlovely = ["Everyone is like a butterfly, they start out ugly and awkward and then morph into beautiful graceful butterflies that everyone loves. - Drew Barrymore", "Love means to love that which is unlovable; or it is no virtue at all. - Gilbert K. Chesterton", "Anyone can love a rose, but it takes a lot to love a leaf. - Tom Flynn", "Real love is when you are completely committed to someone even when they are being completely unlovable. - Dave Willis", "Connection is why we're here; it is what gives purpose and meaning to our lives. The power that connection holds in our lives was confirmed when the main concern about connection emerged as the fear of disconnection; the fear that something we have done or failed to do, something about who we are or where we come from, has made us unlovable and unworthy of connection. - Brené Brown"]
 
 input = nil
 loop do
@@ -16,57 +20,62 @@ loop do
   end
 end
 
-name = nil
-def welcome
- puts "What is your name?"
- name = gets.chomp.capitalize
- puts "Hello #{name}! I've got some encouragement for you!"
+
+class Person
+  attr_accessor :name
+  def initialize
+    puts "What is your name?"
+    @name = name
+    @name = gets.chomp.capitalize
+    if name.empty?
+      @name = "Amazing Human"
+    end
+    puts "Hello #{name}! We've got some encouragement for you!"
+  end
 end
 
-welcome
+Person.new
 
 #Is there a way to put this whole loop in one object so I can call it with one word similar to "welcome"?
 
 #I'd like to use someone's name in the script below but I'm not sure how to make it accesible below. I think the local variable of name is limited to the method of "welcome"
 # but I'm unsure how to call it in another method (or in this case the loop below).
 
-loop do
-  puts <<-eos
-  Are you feeling?
-  Anxiety   Depression
-  Discouragement  Unlovely
-  Different Feeling
-  Type your answer below:
-   eos
-  feeling = gets.chomp.downcase
-  case feeling
-  when "anxiety"
-   puts "Anxiety does not empty tomorrow of its sorrows, but only empties today of its strength. -Charles Spurgeon"
-   #I'd like to call an array (eventually randomized) of quotes here for each feeling.
-  when "depression"
-   puts "Depression is the inability to construct a future. -Rollo May"
-  when "discouragement"
-   puts <<-eos
-   In spite of everything I shall rise again: 
-   I will take up my pencil, 
-   which I have forsaken in my great discouragement, 
-   and I will go on with my drawing. 
-   -Vincent Van Gogh
-    eos
-  when "unlovely"
-   puts <<-eos
-   "Everyone is like a butterfly, they start out ugly 
-   and awkward and then morph into beautiful graceful 
-   butterflies that everyone loves." 
-   -Drew Barrymore
-   eos
-  else
-   #feelings.push(answer) #I'd like to add the answered feeling to the array to eventually research quotes for that topic. Think this will be better used when I started using Ruby on Rails.
-   puts "The feeling of #{feeling} will be added to our list and encouragement will be researched for the future. Try choosing one of the feelings listed above."
-  end
-  puts "Do you need more encouragement? (yes/no)"
-  need_more = gets.chomp
-  until need_more == 'yes'
-    abort("Awesome! Hope you're feeling better! Now go encourage someone else!")
+def feeling_help
+  loop do
+    puts <<-eos
+    Are you feeling?
+    Anxiety   Depression
+    Discouragement  Unlovely
+    Feeling Better (Exit)
+    Type your answer below:
+     eos
+    feeling = gets.chomp.downcase
+    case feeling
+    when "anxiety"
+     puts @anxiety.sample
+     puts 
+    when "depression"
+     puts @depression.sample
+     puts 
+    when "discouragement"
+     puts @discouragement.sample
+     puts 
+    when "unlovely"
+      puts @unlovely.sample
+      puts
+    when "feeling better", "better", "exit", "no"
+      abort("Awesome! Hope you're feeling better! Now go encourage someone else!")
+    else
+     puts "We don't have #{feeling} in our list. We know first hand how discouraging this could be. Try choosing one of the feelings listed above as we research more feelings."
+     puts 
+    end
+    puts "Do you need more encouragement? (yes/no)"
+    need_more = gets.chomp
+    until need_more == 'yes'
+      abort("Awesome! Hope you're feeling better! Now go encourage someone else!")
+    end
   end
 end
+
+feeling_help
